0eee56f73a2046b835eecd0e3c4ab227d514ff4c
tohuy2710 <tohuy2710@gmail.com>
2024-05-08 14:39:07 +0700

    update

--- week2/bt1.cpp
+++ week2/bt1.cpp
@@ -1,37 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-struct point{
-int x, y;
-point(int _x, int _y){
-    x = _x;
-    y = _y;
+bool isPrime(int n){
+    if(n < 2) return 0;
+    for(int i = 2; i < sqrt(n); i++){
+        if(n%i==0) return 0;
+    }
+    return 1;
 }
-point();
-};
-struct rect{
-int x, y, w, h;
-
-rect(int _x, int _y, int _w, int _h){
-    x = _x; y = _y; w = _w; h = _h;
-}
-rect();
-bool contain(point p){
-    int px = p.x, py = p.y;
-    if(px >= x && px <= x + h && py >= y && py <= y + h) return 1;
-    return 0;
-}
-
-
-};
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
-    int x, y, w, h; cin >> x >> y >> w >> h;
-    rect r(x,y,w,h);
-    cin >> x >> y;
-    point p(x, y);
-    cout << r.contain(p);
+    
+    int n;
+    cin >> n;
+    cout << isPrime(n);
     return 0;
 }
--- week3/bt1.cpp
+++ week3/bt1.cpp
@@ -1,17 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-// int a[3] = {1,2,3, 4};
-int a[] = {1,2,3,4};
+struct point{
+int x, y;
+point(int _x, int _y){
+    x = _x;
+    y = _y;
+}
+point();
+};
+struct rect{
+int x, y, w, h;
+
+rect(int _x, int _y, int _w, int _h){
+    x = _x; y = _y; w = _w; h = _h;
+}
+rect();
+bool contain(point p){
+    int px = p.x, py = p.y;
+    if(px >= x && px <= x + h && py >= y && py <= y + h) return 1;
+    return 0;
+}
+
+
+};
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
-    
-    int a[] = {1,2,3,4};
-    // int a[3] = {1,2,3,4};
-    for(int x : a){
-        cout << x << " ";
-    }
+    int x, y, w, h; cin >> x >> y >> w >> h;
+    rect r(x,y,w,h);
+    cin >> x >> y;
+    point p(x, y);
+    cout << r.contain(p);
     return 0;
 }
